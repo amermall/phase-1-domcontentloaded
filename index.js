4ea68c218e195b0f3ead15c5b15add01e6d16039
Amer Mall <amer@icloud.com>
2022-01-29 23:28:03 -0500

    commit tree 92044bc58a5095c6b18b511cebff4f78e51c6bfe

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  // console.log("The DOM has loaded");
+
+  // Use the event's callback function to target the paragraph element with id="text" and replace the text with "This is really cool!"
+  // 1. Use a call back function"
+  updateParagraphText();
+  
+});
+
+function updateParagraphText() {
+  // console.log(document.querySelector("#text"));
+  const textToUpdate = document.querySelector("#text").textContent = "This is really cool!";
+  // console.log(textToUpdate);
+
+  return textToUpdate;
+}
